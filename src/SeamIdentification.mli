2ee485072399e227b3cab2c311ca32b88ba99178
EmilyEljamal <eeljama1@jh.edu>
2024-11-16 15:41:17 -0500

    added unimplemented mls

--- src/SeamIdentification.mli
+++ src/SeamIdentification.mli
@@ -1,11 +1,11 @@
-
+open Types
 
 (** [calc_minimal_energy_to_bottom energy_map] calculates the minimal energy required 
     to reach the bottom from each pixel in the energy map. memoized function. *)
-val calc_minimal_energy_to_bottom: Minimal_energy_map -> Minimal_energy_map
+val calc_minimal_energy_to_bottom: energy_map -> Minimal_energy_map.t
 
 (** [find_vertical_seam minimal_energy_map] finds the vertical seam with minimal energy. *)
-val find_vertical_seam : Minimal_energy_map -> int list
+val find_vertical_seam : Minimal_energy_map.t -> int list
 
 (** [remove_vertical_seam image seam] removes the specified vertical seam from the image. *)
 val remove_vertical_seam : image -> int list -> image
---
+++ src/Types.ml
@@ -0,0 +1,57 @@
+module Pair = struct
+  type t = {
+    energy : float;
+    direction : int;
+  } [@@deriving compare]
+
+  let create (_energy : float) (_direction : int) : t =
+    failwith "Not implemented: Pair.create"
+
+  let get_energy (_pair : t) : float =
+    failwith "Not implemented: Pair.get_energy"
+
+  let get_direction (_pair : t) : int =
+    failwith "Not implemented: Pair.get_direction"
+
+  let update_energy (_pair : t) (_energy : float) : t =
+    failwith "Not implemented: Pair.update_energy"
+end
+
+module Array_2d = struct
+  type 'a t = 'a array array
+
+  let init (_rows : int) (_cols : int) (_f : int -> int -> 'a) : 'a t =
+    failwith "Not implemented: Array_2d.init"
+
+  let get (_arr : 'a t) (_x : int) (_y : int) : 'a =
+    failwith "Not implemented: Array_2d.get"
+
+  let dimensions (_arr : 'a t) : int * int =
+    failwith "Not implemented: Array_2d.dimensions"
+
+  let adjacents (_arr : 'a t) (_x : int) (_y : int) : 'a list =
+    failwith "Not implemented: Array_2d.adjacents"
+
+  let map (_f : 'a -> 'b) (_arr : 'a t) : 'b t =
+    failwith "Not implemented: Array_2d.map"
+end
+
+type image = float Array_2d.t
+
+type energy_map = float Array_2d.t
+
+module Minimal_energy_map = struct
+  type t = Pair.t Array_2d.t
+
+  let from_energy_map (_energy_map : energy_map) : t =
+    failwith "Not implemented: Minimal_energy_map.from_energy_map"
+
+  let get_minimal_energy (_map : t) (_row : int) : int =
+    failwith "Not implemented: Minimal_energy_map.get_minimal_energy"
+
+  let update_direction (_map : t) (_row : int) (_col : int) (_direction : int) : unit =
+    failwith "Not implemented: Minimal_energy_map.update_direction"
+
+  let to_energy_map (_map : t) : energy_map =
+    failwith "Not implemented: Minimal_energy_map.to_energy_map"
+end
