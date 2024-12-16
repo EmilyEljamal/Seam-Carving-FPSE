7fe1cd82dffaa758c8f7d3097e5329156452e464
EmilyEljamal <eeljama1@jh.edu>
2024-12-16 17:27:44 -0500

    main fixed

--- bin/main.ml
+++ bin/main.ml
@@ -4,26 +4,14 @@ open Core
 
 let () = 
   match Sys.get_argv () |> Array.to_list with
-  | _ :: input_path :: output_path :: [] -> 
-    ( 
-      let mask =
-        let rows = List.init 25 ~f:(fun i -> i + 125) in
-        let cols = List.init 25 ~f:(fun i -> i + 147) in
-        List.concat (List.map rows ~f:(fun x -> List.map cols ~f:(fun y -> (x, y)))) in
+  | _ :: input_path :: num_seams_str :: output_path :: [] -> 
+    (
+      let num_seams = int_of_string num_seams_str in
       let original_image = Image_process.ImageProcess.load_image input_path in 
-      Printf.printf "Image loaded\n";
-      let seams, result_images = Image_process.ImageProcess.remove_object original_image mask [] in
-      let final_image = Option.value_exn (List.hd (List.rev result_images)) in 
-      let first_image = List.hd result_images in
-      let original_rows, original_cols = Types.Array_2d.dimensions (Option.value_exn first_image) in
-      let new_rows, new_cols = Types.Array_2d.dimensions ( final_image) in 
-      let updated_images_with_seams = Image_process.ImageProcess.add_seams final_image original_cols new_cols  in (* Combine all resulting images into one list *) 
-      let all_result_images = result_images @ updated_images_with_seams in
-      (*let result_images = Image_process.ImageProcess.remove_seams original_image num_seams in
-      Still need to process these modified images as a gif then save to user's output path *)
-      Gif.Gif.make_gif all_result_images output_path
-
-      ) (* Temporary unit placeholder *)
+      let result_images = Image_process.ImageProcess.remove_seams original_image num_seams in
+      (* Still need to process these modified images as a gif then save to user's output path *)
+      Gif.Gif.make_gif result_images output_path; (* Temporary unit placeholder *)
+    )
 | _ ->
   Printf.printf "No image path provided" 
 
