1593ea82fcc0d6d48dff8af7c1bc99e53bceae37
Maria Herne <noelia.herne@gmail.com>
2024-12-16 20:57:21 -0500

    working app with black pixels in image

--- bin/main.ml
+++ bin/main.ml
@@ -4,6 +4,24 @@ open Core
 
 let () = 
   match Sys.get_argv () |> Array.to_list with
+  | _ :: input_path :: num_seams_str :: output_path :: [] -> 
+    (
+      let num_seams = int_of_string num_seams_str in
+      let original_image = Image_process.ImageProcess.load_image input_path in 
+      let result_images = Image_process.ImageProcess.remove_seams original_image num_seams in
+      (* Still need to process these modified images as a gif then save to user's output path *)
+      Gif.Gif.make_gif result_images output_path
+      )
+| _ ->
+  Printf.printf "No image path provided" 
+
+
+
+
+(* OBJECT REMOVAL MAIN ---  *)
+(* 
+let () = 
+  match Sys.get_argv () |> Array.to_list with
   | _ :: input_path :: output_path :: [] -> 
     ( 
       let mask =
@@ -25,7 +43,7 @@ let () =
 
       ) (* Temporary unit placeholder *)
 | _ ->
-  Printf.printf "No image path provided" 
+  Printf.printf "No image path provided"  *)
 
 (*
   load image
